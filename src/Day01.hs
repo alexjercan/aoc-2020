eab9da8da587c60705b7113772f4076b30808a26
Alex Jercan <jercan_alex27@yahoo.com>
2023-11-06 09:01:08 +0200

    add day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,14 @@
 module Day01 (main) where
 
 part1 :: String -> String
-part1 = id
+part1 input = show $ head [x * y | x <- nums, y <- nums, x + y == 2020]
+  where
+    nums = map read $ lines input :: [Int]
 
 part2 :: String -> String
-part2 = const ""
+part2 input = show $ head [x * y * z | x <- nums, y <- nums, z <- nums, x + y + z == 2020]
+  where
+    nums = map read $ lines input :: [Int]
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input
