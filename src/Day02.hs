d9c746df7a8d4cf9586908cde713e230937e9ef0
Alex Jercan <jercan_alex27@yahoo.com>
2023-11-06 09:33:12 +0200

    day2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,10 +1,30 @@
 module Day02 (main) where
 
+valid1 :: String -> Bool
+valid1 password =
+    let (min', rest) = break (== '-') password in
+    let (max', rest') = break (== ' ') $ tail rest in
+    let (letter, rest'') = break (== ':') $ tail rest' in
+    let password' = tail $ tail rest'' in
+    let count = length $ filter (== head letter) password' in
+    count >= read min' && count <= read max'
+
 part1 :: String -> String
-part1 = id
+part1 = show . length . filter valid1 . lines
+
+valid2 :: String -> Bool
+valid2 password =
+    let (pos1', rest) = break (== '-') password in
+    let (pos2', rest') = break (== ' ') $ tail rest in
+    let (letter, rest'') = break (== ':') $ tail rest' in
+    let password' = tail $ tail rest'' in
+    let pos1 = read pos1' - 1 in
+    let pos2 = read pos2' - 1 in
+    let letter' = head letter in
+    (password' !! pos1 == letter') /= (password' !! pos2 == letter')
 
 part2 :: String -> String
-part2 = const ""
+part2 = show . length . filter valid2 . lines
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input
