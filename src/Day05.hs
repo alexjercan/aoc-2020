c5d45cc94ac0d97cdf3c578ed6daf295f977fbba
Alex Jercan <jercan_alex27@yahoo.com>
2023-11-09 21:15:54 +0200

    added day 05

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,10 +1,25 @@
 module Day05 (main) where
+import Data.List (sort)
+
+range :: (Int, Int) -> Char -> (Int, Int)
+range (low, high) 'F' = (low, low + (high - low) `div` 2)
+range (low, high) 'B' = (low + (high - low) `div` 2 + 1, high)
+range (low, high) 'L' = (low, low + (high - low) `div` 2)
+range (low, high) 'R' = (low + (high - low) `div` 2 + 1, high)
+range _ _ = error "Invalid input"
+
+seatId :: String -> Int
+seatId seat = row * 8 + col
+  where
+    (row, _) = foldl range (0, 127) $ take 7 seat
+    (col, _) = foldl range (0, 7) $ drop 7 seat
 
 part1 :: String -> String
-part1 = id
+part1 = show . maximum . map seatId . lines
 
 part2 :: String -> String
-part2 = const ""
+part2 = show . missing . map seatId . lines
+    where missing seats = head [x | x <- [minimum seats..maximum seats], x `notElem` seats]
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input
