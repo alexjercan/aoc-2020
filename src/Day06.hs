72685b904c4ec98b275ba8102e9e175b89ed340e
Alex Jercan <jercan_alex27@yahoo.com>
2023-11-10 14:03:51 +0200

    day 06

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,10 +1,21 @@
 module Day06 (main) where
 
+import Data.List.Split (splitOn)
+import Data.List (nub)
+import Data.Char (isLetter)
+
+questionsYesAny :: String -> Int
+questionsYesAny = length . nub . filter isLetter
+
 part1 :: String -> String
-part1 = id
+part1 = show . sum . map questionsYesAny . splitOn "\n\n"
+
+questionsYesAll :: String -> Int
+questionsYesAll group = length $ filter (\c -> all (elem c) people) ['a'..'z']
+    where people = lines group
 
 part2 :: String -> String
-part2 = const ""
+part2 = show . sum . map questionsYesAll . splitOn "\n\n"
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input
